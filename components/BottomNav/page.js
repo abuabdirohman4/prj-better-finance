ef86931b9be7472c7c3205c113f053dcba373903
abuabdirohman4 <abuabdirohman4@gmail.com>
2024-06-09 11:53:22 +0700

    bottomNav : add + button

--- components/BottomNav/page.js
+++ components/BottomNav/page.js
@@ -21,6 +21,38 @@ export default function BottomNav() {
             Transactions
           </span>
         </Link>
+        <div className="flex items-center justify-center">
+          <Link
+            data-tooltip-target="tooltip-new"
+            href=""
+            className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+          >
+            <svg
+              className="w-4 h-4 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 1v16M1 9h16"
+              />
+            </svg>
+            <span className="sr-only">New item</span>
+          </Link>
+        </div>
+        <div
+          id="tooltip-new"
+          role="tooltip"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+        >
+          Create new item
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
         <Link
           href={"/budgets"}
           className="inline-flex flex-col items-center justify-center w-32 px-5 border-e border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-800 group dark:border-gray-600"
