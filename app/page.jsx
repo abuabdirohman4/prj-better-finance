0ed00024948bc1986fc781250b7c2973f69f9e44
abuabdirohman4 <abuabdirohman4@gmail.com>
2024-06-20 07:00:53 +0700

    storage : add clear storage on root

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,8 @@
+'use client'
+import { clearLocal } from "@/utils/session";
+
 export default function Root() {
+  clearLocal()
   return (
     <div className="flex h-screen flex-col justify-evenly bg-cover bg-top bg-no-repeat px-9"></div>
   );
