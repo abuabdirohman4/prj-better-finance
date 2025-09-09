0397e0d9d870066e80a4df96aa3d3135b50d9254
Abu Abdirohman <abuabdirohman4@gmail.com>
2025-09-09 11:14:55 +0700

    feat: add Prettier for code formatting with new scripts for formatting and checking

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Basic formatting
+  semi: true,
+  singleQuote: false,
+  trailingComma: 'es5',
+  tabWidth: 4,
+  useTabs: false,
+  //   printWidth: 80,
+  endOfLine: 'lf',
+
+  // JavaScript/JSX formatting
+  jsxSingleQuote: false,
+  //   arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
